c72db4c7d43f05bb3bda9101038aef8b4bcad0d3
as <>
2020-08-27 01:47:45 +0900

    いいねの状態をjQueryで反映

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -47,7 +47,10 @@ document.addEventListener('turbolinks:load', () => {
     $('.inactive-heart').on('click', () => {
         axios.post(`/articles/${articleId}/like`)
         .then((response) => {
-            console.log(response)
+            if (response.data.status === 'ok') {
+                $('.active-heart').removeClass('hidden')
+                $('.inactive-heart').addClass('hidden')
+            }
         })
         .catch((e) => {
             window.alert('Error')
@@ -58,8 +61,12 @@ document.addEventListener('turbolinks:load', () => {
     $('.active-heart').on('click', () => {
         axios.delete(`/articles/${articleId}/like`)
         .then((response) => {
-            console.log(response)
+            if (response.data.status === 'ok') {
+                $('.active-heart').addClass('hidden')
+                $('.inactive-heart').removeClass('hidden')
+            }
         })
+        
         .catch((e) => {
             window.alert('Error')
             console.log(e)
